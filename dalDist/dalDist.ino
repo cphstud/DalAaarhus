b6a42a3eb80839efac1685980010d8149acba6a5
cphstud <studcph@gmail.com>
2021-11-11 08:22:03 +0100

    serial

--- dalDist/dalDist.ino
+++ dalDist/dalDist.ino
@@ -10,8 +10,8 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
+  //al.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  //Serial.println("with Arduino UNO R3");
 }
 void loop() {
   // Clears the trigPin condition
@@ -19,14 +19,14 @@ void loop() {
   delayMicroseconds(2);
   // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
+  delay(100);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
   // Displays the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  //Serial.print("Distance: ");
+  Serial.println(distance);
+  //Serial.println(" cm");
 }
